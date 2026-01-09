62b586279d10eca6c33e28966c9ce9002f79f065
dishachotai <dishachotai58@gmail.com>
2026-01-09 12:10:53 +0530

    Update 21.factorial_using_recursion.c

--- C_Program/21.factorial_using_recursion.c
+++ C_Program/21.factorial_using_recursion.c
@@ -1,25 +1,20 @@
 //21 WAP to find factorial using recursion
+//Recursion is a programming technique in which a function calls 
+//itself to solve a problem until a base condition is reached.
 #include<stdio.h>
-int factorial(int n)
+int factorial(int n)//function
 {
-    int i,result=1;
-
-    for(i=1;i<=n;i++)
-    {
-        result=result*i;
-    }
-    return result;
+    if(n==0||n==1)
+        return 1;           
+	else
+        return n*factorial(n-1); // recursive call
 }
 int main()
 {
     int num,fact;
-
     printf("Enter a number: ");
-    scanf("%d", &num);
+    scanf("%d", &num);    
+    fact = factorial(num);  // function call
+    printf("Factorial of %d is %d", num,fact);
 
-    fact=factorial(num); // Function Call
-
-    printf("Factorial of %d is %d\n", num,fact);
 }
-
-

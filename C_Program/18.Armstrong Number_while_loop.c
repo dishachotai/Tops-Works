9fa7a4ded8fb08aced17e71d9ea9eb2fe7c6011d
dishachotai <dishachotai58@gmail.com>
2026-01-09 11:55:11 +0530

    Update 18.Armstrong Number_while_loop.c

--- C_Program/18.Armstrong Number_while_loop.c
+++ C_Program/18.Armstrong Number_while_loop.c
@@ -1,21 +1,22 @@
-//18 Program of Armstrong Number in C Using While Loop
+//18 Program of Armstrong Number in C Using For Loop
 #include<stdio.h>
 int main()
 {
-    int num,temp,r,sum=0;
+    int num,temp,r,sum;
     printf("Enter Number: ");
-    scanf("%d",&num);
-	
-	temp=num;
-    sum=0;  // reset sum
-    while(temp>0)
+    scanf("%d", &num);
+     // FOR LOOP to process each digit of the number
+    // temp=num(copy original number)
+    //temp>0(run till all digits are processed)
+    //temp=temp/10(remove last digit each time)
+    
+	temp = num;
+	for(temp=num;temp>0;temp=temp/10)
     {
-        r=temp%10;
-        sum=sum+(r*r*r);
-        temp=temp/10;
+        r=temp%10;// extract last digit
+        sum=sum+(r*r*r); // add cube of digit to sum
     }
-
-    if(sum==num)
+    if(sum == num)
         printf("%d is an Armstrong Number ", num);
     else
         printf("%d is not an Armstrong Number ", num);

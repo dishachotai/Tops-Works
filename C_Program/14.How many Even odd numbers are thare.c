87c0559ee2028215d3974a5ef88ca843bd0c9dc5
dishachotai <dishachotai58@gmail.com>
2026-01-09 11:41:22 +0530

    Update 14.How many Even odd numbers are thare.c

--- C_Program/14.How many Even odd numbers are thare.c
+++ C_Program/14.How many Even odd numbers are thare.c
@@ -2,29 +2,25 @@
 //numbers are there b. How many odd numbers are there c. Sum of even
 //numbers d. Sum of odd numbers?
 #include<stdio.h>
-main()
+int main()
 {
-	
-	int num, i, evencount = 0, oddcount = 0, evensum = 0, oddsum = 0;
+	int num,i,evencount,oddcount,evensum,oddsum;
 	printf("Enter 10 numbers:\n");
-	for(i=1; i<=10; i++)
+	for(i=1;i<=10;i++)
 	{
 		scanf("%d",&num);
-	
-		if(num % 2==0)
+		if(num%2==0)
 		{
-			evencount++;
-			evensum=evensum + num;
+			evencount++;			 		// Increase even count
+           evensum=evensum+num;				// Add number to even sum 
 		}
 		else{
-			oddcount++;
-			oddsum=oddsum+num;
+			oddcount++; 					// Increase odd count
+			oddsum=oddsum+num;				// Add number to add sum 
 		}
 	}
 	printf("Total Even Numbers: %d\n",evencount);
 	printf("Total odd Numbers :%d\n",oddcount);
 	printf("sum of Even numbers :%d\n",evensum);
 	printf("sum of odd Numbers :%d\n",oddsum);
-	
-	
 }

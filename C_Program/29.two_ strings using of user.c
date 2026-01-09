116db02dadc9f25e8eae9f9daaace968b6f6fa46
dishachotai <dishachotai58@gmail.com>
2026-01-09 12:21:19 +0530

    Update 29.two_ strings using of user.c

--- C_Program/29.two_ strings using of user.c
+++ C_Program/29.two_ strings using of user.c
@@ -1,19 +1,30 @@
+//29 Program to join two strings using a user-defined function
+// without using inbuilt string functions like strcat()
 #include <stdio.h>
+void joinStrings(char s1[],char s2[])
+{
+    int i=0,j=0;
 
-int main() {
-    char str[100];
-    int i = 0;
-
-    printf("Enter a string: ");
-    scanf("%s", str);
-
-    // Calculate length manually
-    while(str[i] != '\0') {
-        i++;
+    while(s1[i]!='\0')
+    {
+        i++; 
     }
-
-    printf("Length of string: %d\n", i);
-
-    return 0;
+    // Copy characters
+    while(s2[j]!='\0')
+    {
+        s1[i]=s2[j];  // append character
+        i++;
+        j++;
+    }
+    s1[i]='\0';   // add null character at the end
 }
-
+int main()
+{
+    char str1[100], str2[50];
+    printf("Enter first string: ");
+    scanf("%s", str1);
+    printf("Enter second string: ");
+    scanf("%s", str2);
+    joinStrings(str1,str2);   // function call
+    printf("Joined String: %s\n",str1);
+}

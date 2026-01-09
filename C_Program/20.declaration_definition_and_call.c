57404f348be215004c9208efe8b916e7b98b40b3
dishachotai <dishachotai58@gmail.com>
2026-01-09 12:07:11 +0530

    Update 20.declaration_definition_and_call.c

--- C_Program/20.declaration_definition_and_call.c
+++ C_Program/20.declaration_definition_and_call.c
@@ -1,27 +1,24 @@
 //20  Write a C program that calculates the factorial of a number using a 
 //function Include function declaration, definition, and call. 
 #include<stdio.h>
-int factorial(int n)	
+int factorial(int n)//function	
 {
     int i,result=1;
-
     for(i=1;i<=n;i++)
     {
-        result=result*i;
+        result=result*i;// multiply current number with result
     }
-    return result;
+    return result;//calling function
 }
 
 int main()
 {
     int num, fact;
-
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    fact = factorial(num); // Function Call
+    fact=factorial(num); // Function Call
 
     printf("Factorial of %d is %d\n",num,fact);
 }
-
 

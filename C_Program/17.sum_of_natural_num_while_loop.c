57cfafffefbf2662bd10b045e28c65997c4d07ce
dishachotai <dishachotai58@gmail.com>
2026-01-09 11:44:40 +0530

    Update 17.sum_of_natural_num_while_loop.c

--- C_Program/17.sum_of_natural_num_while_loop.c
+++ C_Program/17.sum_of_natural_num_while_loop.c
@@ -5,7 +5,7 @@ int main()
 	int n,i=1,sum=0;
 	printf("Enter number ");
 	scanf("%d",&n);
-	
+		// Loop runs while value of i is less than or equal to n
 	while(i<=n)
 	{
 		sum=sum+i;
@@ -14,3 +14,4 @@ int main()
 	    printf("Sum of %d natural numbers = %d", n, sum);
 
 }
+

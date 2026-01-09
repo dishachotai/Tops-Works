2e679b9cb7258597ea2db3fad652ecf2f2638d44
dishachotai <dishachotai58@gmail.com>
2026-01-09 11:26:50 +0530

    Update 11.maximum number among 3 numbers.c

--- C_Program/11.maximum number among 3 numbers.c
+++ C_Program/11.maximum number among 3 numbers.c
@@ -5,6 +5,11 @@ main()
 	int a,b,c,max;
 	printf("Enter three number:");
 	scanf("%d %d %d",&a,&b,&c);
+	//	condition ? value_if_true : value_if_false;
+	// (b > a) ? checks if b is greater than a
+    // If TRUE ? compare a and c ? (a > c ? a : c)
+    // If FALSE ? compare b and c ? (b > c ? b : c)
 	max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
 	printf("maximum number is: %d", max);
 }
+
